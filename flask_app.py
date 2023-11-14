4edd7329b579e440072f6d2e4eb3a334469bc3e5
hramzan01 <112130674+hramzan01@users.noreply.github.com>
2023-11-14 16:07:04 +0000

    updated to show plotly chart at top

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import sqlite3
 import pandas as pd
+import plotly.express as px
 
 app = Flask(__name__)
 
@@ -22,17 +23,22 @@ def fetch_table_data(table_name):
     conn.close()
     return table_data
 
+def generate_bar_chart(data):
+    fig = px.bar(data, x='Rating System')
+    return fig.to_html(full_html=False)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     tables = fetch_tables()
     table_data = None
+    bar_chart = None
 
     if request.method == "POST":
         selected_table = request.form["table"]
         table_data = fetch_table_data(selected_table)
+        bar_chart = generate_bar_chart(table_data)
 
-    return render_template("index.html", tables=tables, table_data=table_data)
+    return render_template("index.html", tables=tables, table_data=table_data, bar_chart=bar_chart)
 
 if __name__ == "__main__":
     app.run()
