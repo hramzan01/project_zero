526e8dc2eb1651f8ac50b2ec5339cf8f7940344b
hramzan01 <112130674+hramzan01@users.noreply.github.com>
2024-01-03 15:15:46 +0000

    added csv extract util

--- etl/utils.py
+++ etl/utils.py
@@ -1,6 +1,9 @@
 from math import radians, sin, cos, asin, sqrt
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+
+
 
 # functions to help in scripts
 
@@ -12,3 +15,12 @@ def facade_area(area):
 # calculate the number of floors based on the height
 def facade_area(height):
     pass
+
+# def extract_csv_from_excel(file,tab)
+def excel_to_csv(path, tab):
+    df = pd.read_excel(path,tab)
+    file = path.replace('.xlsx','.csv').replace('xcl','csv')
+    result = df.to_csv(file)
+    print(f'\n--csv extracted sucessfully as {file}--\n')
+
+    return result 
