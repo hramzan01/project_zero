648850cb8e332fe147f75c2dc7f159c8bd324c40
hramzan01 <112130674+hramzan01@users.noreply.github.com>
2024-01-04 15:32:58 +0000

    updated typologies module and various notebooks

--- etl/typologies.py
+++ etl/typologies.py
@@ -10,3 +10,53 @@ class Typologies:
     def __init__(self):
         # Assign an attribute ".data" to all new instances of Typologies
         self.data = ProjectZero().get_data()
+
+    def get_building_typology(self):
+        # df_model instance
+        df_model = self.data['hz_model'].copy()
+
+        # Drop unecessary columns
+        df_model = df_model.drop(columns=['ID','Colour'])
+
+        # filtering str based columns 
+        columns = list(df_model.columns)
+        str_columns = [column for column in columns if columns != 'Area']
+
+        # replace empty with unassigned
+        for column in str_columns:
+            df_model[column] = df_model[column].replace(' ','')
+        
+        # dropping empty values for building column
+        df_model = df_model[df_model.Building != '']
+
+        # Group by building type 
+        df_grouped = df_model.groupby(['Building', 'Typology','Plot']).sum(False).reset_index().sort_values('Building')
+        df_grouped.Area = df_grouped.Area.astype(int)
+        
+        return df_grouped
+
+    def get_building_facade(self):
+        # df_model instance
+        df_model = self.data['hz_model'].copy()
+
+        # if facade area not in df then generate estimate with utils.estimate_facade_area
+        from etl.utils import estimate_facade_area
+
+        if 'Facade' not in list(df_model.columns):
+            df_model['Facade'] = estimate_facade_area(df_model, 4)
+
+        # Create new data frame
+        df = pd.DataFrame({'building': df_model.Building, 'area': df_model.Area,'facade': df_model.Facade })
+
+        # dropping empty values for building column
+        df = df[df.building != ' ']
+
+        # group and sort
+        df = df.groupby('building').sum(False).reset_index().sort_values('building')
+        df
+
+        # Turn area into int
+        df.area = df.area.astype(int)
+
+        return df
+
--- etl/utils.py
+++ etl/utils.py
@@ -2,19 +2,11 @@ from math import radians, sin, cos, asin, sqrt
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import numpy as np
 
 
 
-# functions to help in scripts
-
-# calculate the facade of a building based on area
-def facade_area(area):
-    pass
-
-
-# calculate the number of floors based on the height
-def facade_area(height):
-    pass
+'''functions to help in scripts'''
 
 # def extract_csv_from_excel(file,tab)
 def excel_to_csv(path, tab):
@@ -24,3 +16,20 @@ def excel_to_csv(path, tab):
     print(f'\n--csv extracted sucessfully as {file}--\n')
 
     return result 
+
+# calculate the facade of a building based on area
+def estimate_facade_area(df: pd.DataFrame, height: int):
+    area = df.Area
+    perimeter = np.sqrt(area) * 4
+    facade = perimeter * height
+    series = df['Facade'] = facade.astype(int)
+
+    return series
+
+# calculate the volume on the height
+def estimate_volume(df: pd.DataFrame, height: int):
+    area = df.Area
+    volume = area * height
+    df['volume_m3'] = volume.astype(int)
+
+    return df
