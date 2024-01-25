8eaa7dcaee95745b24ab3b387de4e60b63ae6049
hramzan01 <112130674+hramzan01@users.noreply.github.com>
2024-01-25 11:06:39 +0000

    included data population to piepline

--- watchdog/watchdog_monitor.py
+++ watchdog/watchdog_monitor.py
@@ -17,25 +17,31 @@ class CustomEventHandler(FileSystemEventHandler):
             # Run the script only when the specified CSV file is modified
             self.convert_design_data()
 
+
+# Functions to process the design data once a modified csv has been detected
+    
     def convert_design_data(self):
         import os
         import pandas as pd
 
+        # ////////////////////////////////////////////////////////////////////// READING DATA 
 
         # # define csv path
         script_directory = os.path.dirname(os.path.abspath(__file__))
-        parent_directory = os.path.abspath(os.path.join(script_directory, '..', '..', '..'))
+        # parent_directory = os.path.abspath(os.path.join(script_directory, '..', '..', '..')) # uncomment for packaging 
+        parent_directory = os.path.abspath(os.path.join(script_directory)) # uncomment for testing
         csv_file_name = 'data_design.csv'
 
         # Construct the full path to the CSV file
         csv_path = os.path.join(parent_directory, csv_file_name)
-
         df = pd.read_csv(csv_path)
         print('----------------------')
-        print('01 reading raw data...')
+        print('reading raw model data...')
 
-        # define preprocessed data
-        def preprocess_design_data(df):
+        # ////////////////////////////////////////////////////////////////////// PROCESSING DATA
+        
+        # 00 Retreiving energy demmand
+        def preprocess_design_data(df): # define preprocessed data for utilities
             
                 # df_model instance
                 design_data = df.copy()
@@ -123,12 +129,8 @@ class CustomEventHandler(FileSystemEventHandler):
                     })
 
                 return preproc_design_data
-
-        # run preprocess
-        result = preprocess_design_data(df)
-
-        # run through pipe
-        def return_energy_demmand(result):
+        result = preprocess_design_data(df)      
+        def return_energy_demmand(result): # run processed de through pipe
             import dill
             
             preproc_design_data = result
@@ -145,26 +147,60 @@ class CustomEventHandler(FileSystemEventHandler):
             preproc_design_data['electricity_demmand'] = pipe.predict(preproc_design_data).astype(int)
 
             return preproc_design_data
+        energy_demmands = return_energy_demmand(result) # define outputs
+        print('processing demmand data...')
 
-        # define output
-        energy_demmands = return_energy_demmand(result)
-        print('02 processing data...')
+        # 01 Retreiving population data
+        def get_building_population(df):
+                
+            # df_model instance
+            df_model = df.copy()
+            df_coef = pd.read_csv(os.path.join(parent_directory, 'data_coefficients.csv')) 
 
-        # export as csvclear
+            # rename and merge tables
+            df_model.rename(columns=lambda x: x.lower(), inplace=True)
+            df_model.typology = df_model.typology.str.lower()
+            df_merged = df_model.merge(df_coef, on='typology',how='left')
 
-        # Construct the full path to the CSV file
-        export_file = 'data_demmand.csv'
-        export_path = os.path.join(parent_directory, export_file)
+            # appying coefficients to area
+            coef_features = ['efficiency', 'parking req', 'residents', 'employees ', 'visitors']
 
-        energy_demmands.to_csv(export_path, index=False)
-        print('03 exported file success ✓')
+            for i in coef_features:
+                df_merged[i] = (df_merged[i]*df_merged.area).fillna(0 )
+                df_merged[i] = df_merged[i].astype(int)
+
+            # rename then return the df
+            df_merged.rename(columns={'efficiency':'gfa'}, inplace=True)
+
+            return df_merged
+        population_data = get_building_population(df)
+        print('processing population data...')
+
+
+        # ////////////////////////////////////////////////////////////////////// EXPORTING DATA    
+
+        # Exporting Energy Demmand
+        export_file_demmand = 'data_demmand.csv'
+        export_path_demmand = os.path.join(parent_directory, export_file_demmand)
+        energy_demmands.to_csv(export_path_demmand, index=False)
+        print('exported data demmand successfuly ✓')
+
+        # Exporting Population Demmand
+        export_file_population = 'data_population.csv'
+        export_path_population = os.path.join(parent_directory, export_file_population)
+        population_data.to_csv(export_path_population, index=False)
+        print('exported data population successfuly ✓')
+    
+
 
 if __name__ == "__main__":
 
     # # define csv path
     script_directory = os.path.dirname(os.path.abspath(__file__))
-    parent_directory = os.path.abspath(os.path.join(script_directory, '..', '..', '..'))
+    # parent_directory = os.path.abspath(os.path.join(script_directory, '..', '..', '..')) # uncomment for packaging 
+    parent_directory = os.path.abspath(os.path.join(script_directory)) # uncomment for testing
     csv_file_name = 'data_design.csv'
+
 
     # Construct the full path to the CSV file
     csv_path = os.path.join(parent_directory, csv_file_name)
