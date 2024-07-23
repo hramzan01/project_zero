8d13217747f1ecfcf6b1b1d07a087e84c2d74466
Haaris Ramzan <Haaris.R@rshp.com>
2024-07-23 17:16:29 +0100

    cleaned up code for speckle test

--- speckle/main.py
+++ speckle/main.py
@@ -1,18 +1,14 @@
-#IMPORT LIBRARIES
-#import streamlit
 import streamlit as st
-#specklepy libraries
 from specklepy.api.client import SpeckleClient
 from specklepy.api.credentials import get_account_from_token
-#import pandas
 import pandas as pd
-#import plotly express
 import plotly.express as px
 import os
 
-API_KEY = os.environ["SPECKLE_API"]
+# API_KEY = os.environ["SPECKLE_API"]
+API_KEY = '5f8abe8b8edaed4f5238e5647861058e4d1af4fbb7'
 
-print(os.environ["SPECKLE_API"])
+# print(os.environ["SPECKLE_API"])
 
 #PAGE CONFIG
 st.set_page_config(
@@ -20,63 +16,42 @@ st.set_page_config(
     page_icon="📊"
 )
 
-#CONTAINERS
-header = st.container()
-input = st.container()
-viewer = st.container()
-report = st.container()
-graphs = st.container()
+
+'''WEB VIEWER'''
+
+# import logo
+st.image('speckle/logo.png', width=200)
 
 #HEADER
-#Page Header
-with header:
-    st.title("Speckle Stream Activity App📈")
-#About info
-with header.expander("About this app🔽", expanded=True):
-    st.markdown(
-        """This is a beginner web app developed using Streamlit. My goal was to understand how to interact with Speckle API using SpecklePy, 
-        analyze what is received and its structure. This was easy and fun experiment. 
-        """
-    )
-with input:
-    st.subheader("Inputs")
+st.title("PROJECT ZERO: Analytics ")
 
-    #-------
-    #Columns for inputs
-    serverCol, tokenCol = st.columns([1,3])
-    #-------
-    
-    #-------
-	#User Input boxes
-    speckleServer = serverCol.text_input("Server URL", "speckle.xyz", help="Speckle server to connect.")
-    speckleToken = tokenCol.text_input("Speckle token", API_KEY , help="If you don't know how to get your token, take a look at this [link](<https://speckle.guide/dev/tokens.html>)👈")
-    #-------
-    
-    	#-------
-    #CLIENT
-    client = SpeckleClient(host=speckleServer)
-    #Get account from Token
-    account = get_account_from_token(speckleToken, speckleServer)
-    #Authenticate
-    client.authenticate_with_account(account)
-    #-------
-    
+
 #-------
-    #Streams List👇
-    streams = client.stream.list()
-    #Get Stream Names
-    streamNames = [s.name for s in streams]
-    #Dropdown for stream selection
-    sName = st.selectbox(label="Select your stream", options=streamNames, help="Select your stream from the dropdown")
+#User Input boxes
+speckleServer = 'speckle.xyz'
+speckleToken = API_KEY
+#-------
 
-	#SELECTED STREAM ✅
-    stream = client.stream.search(sName)[0]
-    
-    #Stream Branches 🌴
-    branches = client.branch.list(stream.id)
-    #Stream Commits 🏹
-    commits = client.commit.list(stream.id, limit=100)
-    #-------
+#-------
+#CLIENT
+client = SpeckleClient(host=speckleServer)
+account = get_account_from_token(speckleToken, speckleServer)
+client.authenticate_with_account(account)
+#-------
+
+#-------
+#Streams List👇
+streams = client.stream.list()
+streamNames = [s.name for s in streams]
+sName = st.selectbox(label="Select your stream", options=streamNames, help="Select your stream from the dropdown")
+stream = client.stream.search(sName)[0]
+
+
+#Stream Branches 🌴
+branches = client.branch.list(stream.id)
+#Stream Commits 🏹
+commits = client.commit.list(stream.id, limit=100)
+#-------
     
     #--------------------------
 # Create a definition that generates an iframe from commit id
@@ -87,121 +62,126 @@ def commit2viewer(stream, commit, height=400) -> str:
 
 #--------------------------
 #VIEWER👁‍🗨
-with viewer:
-    st.subheader("Latest Commit👇")
-    commit2viewer(stream, commits[0])
+commit2viewer(stream, commits[0])
 #--------------------------
 
-with report:
-    st.subheader("Statistics")
 
-    #-------
-    # Columns for Cards
-    branchCol, commitCol, connectorCol, contributorCol = st.columns(4)
-    #-------
-    
-        #--------------------------
-    #DEFINITIONS
-    #create a definition to convert your list to markdown
-    def listToMarkdown(list, column):
-        list = ["- " + i + " \\n" for i in list]
-        list = "".join(list)
-        return column.markdown(list)
-    #--------------------------
 
-        #-------
-    #Branch Card 💳
-    branchCol.metric(label = "Number of branches", value= stream.branches.totalCount)
-    #branch names as markdown list
-    branchNames = [b.name for b in branches]
-    listToMarkdown(branchNames, branchCol)
-    #-------
 
-    #-------
-    #Commit Card 💳
-    commitCol.metric(label = "Number of commits", value= len(commits))
-    #-------
+
+
+# '''USER REPORT📊'''
+
+# with report:
+#     st.subheader("Statistics")
+
+#     #-------
+#     # Columns for Cards
+#     branchCol, commitCol, connectorCol, contributorCol = st.columns(4)
+#     #-------
     
-        #-------
-    #Connector Card 💳
-    #connector list
-    connectorList = [c.sourceApplication for c in commits]
-    #number of connectors
-    connectorCol.metric(label="Number of connectors", value= len(dict.fromkeys(connectorList)))
-    #get connector names
-    connectorNames = list(dict.fromkeys(connectorList))
-    #convert it to markdown list
-    listToMarkdown(connectorNames, connectorCol)
-    #-------
+#         #--------------------------
+#     #DEFINITIONS
+#     #create a definition to convert your list to markdown
+#     def listToMarkdown(list, column):
+#         list = ["- " + i + " \\n" for i in list]
+#         list = "".join(list)
+#         return column.markdown(list)
+#     #--------------------------
+
+#         #-------
+#     #Branch Card 💳
+#     branchCol.metric(label = "Number of branches", value= stream.branches.totalCount)
+#     #branch names as markdown list
+#     branchNames = [b.name for b in branches]
+#     listToMarkdown(branchNames, branchCol)
+#     #-------
+
+#     #-------
+#     #Commit Card 💳
+#     commitCol.metric(label = "Number of commits", value= len(commits))
+#     #-------
     
-    	  #-------
-    #Contributor Card 💳
-    contributorCol.metric(label = "Number of contributors", value= len(stream.collaborators))
-    #unique contributor names
-    contributorNames = list(dict.fromkeys([col.name for col in stream.collaborators]))
-    #convert it to markdown list
-    listToMarkdown(contributorNames,contributorCol)
-    #-------
+#         #-------
+#     #Connector Card 💳
+#     #connector list
+#     connectorList = [c.sourceApplication for c in commits]
+#     #number of connectors
+#     connectorCol.metric(label="Number of connectors", value= len(dict.fromkeys(connectorList)))
+#     #get connector names
+#     connectorNames = list(dict.fromkeys(connectorList))
+#     #convert it to markdown list
+#     listToMarkdown(connectorNames, connectorCol)
+#     #-------
     
-    #--------------------------
-with graphs:
-    st.subheader("Graphs")
-    #COLUMNS FOR CHARTS
-    branch_graph_col, connector_graph_col, collaborator_graph_col = st.columns([2,1,1])
+#     	  #-------
+#     #Contributor Card 💳
+#     contributorCol.metric(label = "Number of contributors", value= len(stream.collaborators))
+#     #unique contributor names
+#     contributorNames = list(dict.fromkeys([col.name for col in stream.collaborators]))
+#     #convert it to markdown list
+#     listToMarkdown(contributorNames,contributorCol)
+#     #-------
     
-    		#-------
-    #BRANCH GRAPH 📊
-    #branch count dataframe
-    branch_counts = pd.DataFrame([[branch.name, branch.commits.totalCount] for branch in branches])
-    #rename dataframe columns
-    branch_counts.columns = ["branchName", "totalCommits"]
-    #create graph
-    branch_count_graph = px.bar(branch_counts, x=branch_counts.branchName, y=branch_counts.totalCommits, color=branch_counts.branchName, labels={"branchName":"","totalCommits":""})
-    #update layout
-    branch_count_graph.update_layout(
-        showlegend = False,
-        margin = dict(l=1,r=1,t=1,b=1),
-        height=220)
-    #show graph
-    branch_graph_col.plotly_chart(branch_count_graph, use_container_width=True)
-    #-------
+#     #--------------------------
+# with graphs:
+#     st.subheader("Graphs")
+#     #COLUMNS FOR CHARTS
+#     branch_graph_col, connector_graph_col, collaborator_graph_col = st.columns([2,1,1])
     
-    		#-------
-    #CONNECTOR CHART 🍩
-    commits= pd.DataFrame.from_dict([c.dict() for c in commits])
-    #get apps from commits
-    apps = commits["sourceApplication"]
-    #reset index
-    apps = apps.value_counts().reset_index()
-    #rename columns
-    apps.columns=["app","count"]
-    #donut chart
-    fig = px.pie(apps, names=apps["app"],values=apps["count"], hole=0.5)
-    #set dimensions of the chart
-    fig.update_layout(
-        showlegend=False,
-        margin=dict(l=1, r=1, t=1, b=1),
-        height=200,
-        )
-    #set width of the chart so it uses column width
-    connector_graph_col.plotly_chart(fig, use_container_width=True)
-    #-------
+#     		#-------
+#     #BRANCH GRAPH 📊
+#     #branch count dataframe
+#     branch_counts = pd.DataFrame([[branch.name, branch.commits.totalCount] for branch in branches])
+#     #rename dataframe columns
+#     branch_counts.columns = ["branchName", "totalCommits"]
+#     #create graph
+#     branch_count_graph = px.bar(branch_counts, x=branch_counts.branchName, y=branch_counts.totalCommits, color=branch_counts.branchName, labels={"branchName":"","totalCommits":""})
+#     #update layout
+#     branch_count_graph.update_layout(
+#         showlegend = False,
+#         margin = dict(l=1,r=1,t=1,b=1),
+#         height=220)
+#     #show graph
+#     branch_graph_col.plotly_chart(branch_count_graph, use_container_width=True)
+#     #-------
     
-    		#-------
-    #COLLABORATOR CHART 🍩
-    #get authors from commits
-    authors = commits["authorName"].value_counts().reset_index()
-    #rename columns
-    authors.columns=["author","count"]
-    #create our chart
-    authorFig = px.pie(authors, names=authors["author"], values=authors["count"],hole=0.5)
-    authorFig.update_layout(
-        showlegend=False,
-        margin=dict(l=1,r=1,t=1,b=1),
-        height=200,
-        yaxis_scaleanchor="x",)
-    collaborator_graph_col.plotly_chart(authorFig, use_container_width=True)   
-    #-------
+#     		#-------
+#     #CONNECTOR CHART 🍩
+#     commits= pd.DataFrame.from_dict([c.dict() for c in commits])
+#     #get apps from commits
+#     apps = commits["sourceApplication"]
+#     #reset index
+#     apps = apps.value_counts().reset_index()
+#     #rename columns
+#     apps.columns=["app","count"]
+#     #donut chart
+#     fig = px.pie(apps, names=apps["app"],values=apps["count"], hole=0.5)
+#     #set dimensions of the chart
+#     fig.update_layout(
+#         showlegend=False,
+#         margin=dict(l=1, r=1, t=1, b=1),
+#         height=200,
+#         )
+#     #set width of the chart so it uses column width
+#     connector_graph_col.plotly_chart(fig, use_container_width=True)
+#     #-------
+    
+#     		#-------
+#     #COLLABORATOR CHART 🍩
+#     #get authors from commits
+#     authors = commits["authorName"].value_counts().reset_index()
+#     #rename columns
+#     authors.columns=["author","count"]
+#     #create our chart
+#     authorFig = px.pie(authors, names=authors["author"], values=authors["count"],hole=0.5)
+#     authorFig.update_layout(
+#         showlegend=False,
+#         margin=dict(l=1,r=1,t=1,b=1),
+#         height=200,
+#         yaxis_scaleanchor="x",)
+#     collaborator_graph_col.plotly_chart(authorFig, use_container_width=True)   
+#     #-------
     
             #-------
     # #COMMIT PANDAS TABLE 🔲
