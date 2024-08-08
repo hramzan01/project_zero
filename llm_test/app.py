a044d44a7e219cdb8f00ebf4d7cf30e2bcfa6518
haaris ramzan <h.ramzan01@gmail.com>
2024-08-08 15:54:03 +0100

    updated to clean code

--- llm_test/app.py
+++ llm_test/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from text_retrieval import process_text_file, query_model
 
 # Specify the path to the text file
-TEXT_FILE_PATH = "llm_test/silly.txt"
+TEXT_FILE_PATH = "llm_test/data/kidrock.txt"
 
 def main():
     st.title("Text File Chatbot")
