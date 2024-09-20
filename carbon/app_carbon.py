01802b8e294f2b176fea4711e70d91b78d3c1de2
haaris ramzan <h.ramzan01@gmail.com>
2024-09-20 11:47:29 +0100

    split the speck module further by separating the streams and the object data

--- carbon/app_carbon.py
+++ carbon/app_carbon.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 import pandas as pd
-from speck.main import speck_data, speck_viewer
+from speck.main import speck_streams, speck_data, speck_viewer
 
 
 # Set the page layout
@@ -20,57 +20,59 @@ st.title("PROJECT ZERO: Carbon Calculator")
 
 API_KEY = os.getenv('SPECKLE_API')
 SERVER = 'speckle.xyz'
-STREAM = 'PZ_Beta'
 
-# Load the speckle data
-streams, stream, branches, commits, speckle_object = speck_data(API_KEY, SERVER, STREAM)
+# Load the speckle streams then select a stream
+client, streams = speck_streams(API_KEY, SERVER)
+stream_collection = [str(i).split()[4].replace(',','') for i in streams]
+user_stream = st.selectbox('Select a stream', stream_collection)
 
-stream_collection = [str(i).split()[4] for i in streams]
-selected_stream = st.selectbox('Select a stream', stream_collection)
+if st.button('Load Stream'):
+    st.write('Loading stream...')
+    # Load the speckle data from selected stream
+    stream, branches, commits, speckle_object = speck_data(user_stream, client)
 
-st.write(stream)
-st.write(branches)
-st.write(commits)
-st.write(speckle_object)
+    # Load the speckle viewer
+    speck_viewer(SERVER, stream, commits[0])
 
-# Load the speckle viewer
-speck_viewer(SERVER, stream, commits[0])
-
-
-
-# load the carbon table
-carbon_data = 'carbon/data/ICE_SPICE.csv'
-df = pd.read_csv(carbon_data, skiprows=1)
-
-# df.columns
-# df
+    st.write(stream)
+    st.write(branches)
+    st.write(commits)
+    st.write(speckle_object)
 
 
-materials = df['Material'].unique()
-material = st.selectbox('Select a material', materials)
+    # load the carbon table
+    carbon_data = 'carbon/data/ICE_SPICE.csv'
+    df = pd.read_csv(carbon_data, skiprows=1)
 
-# filter the data based on the material
-df = df[df['Material'] == material]
-df = df[[
-    # 'Material',
-    'Sub-material',
-    'ICE DB Name',
-    'Units of declared unit',
-    'Density of material - kg per m3',
-    'Embodied Carbon per kg (kg CO2e per kg)',
-    'Average Embodied Carbon - kg CO2e per kg']]
-
-st.dataframe(df)
-
-# create plotly chart with embodied carbon per kg by sub-material
-df_chart = df[['Sub-material', 'Average Embodied Carbon - kg CO2e per kg']]
-st.bar_chart(df_chart.set_index('Sub-material'), height=700)
+    # df.columns
+    # df
 
 
+    materials = df['Material'].unique()
+    material = st.selectbox('Select a material', materials)
+
+    # filter the data based on the material
+    df = df[df['Material'] == material]
+    df = df[[
+        # 'Material',
+        'Sub-material',
+        'ICE DB Name',
+        'Units of declared unit',
+        'Density of material - kg per m3',
+        'Embodied Carbon per kg (kg CO2e per kg)',
+        'Average Embodied Carbon - kg CO2e per kg']]
+
+    st.dataframe(df)
+
+    # create plotly chart with embodied carbon per kg by sub-material
+    df_chart = df[['Sub-material', 'Average Embodied Carbon - kg CO2e per kg']]
+    st.bar_chart(df_chart.set_index('Sub-material'), height=700)
 
 
 
-# drop down menu for material selection
-# need to figure out the best way to display the data but how to filter it?
 
-# st.write('This is a test')
+
+    # drop down menu for material selection
+    # need to figure out the best way to display the data but how to filter it?
+
+    # st.write('This is a test')
--- speck/main.py
+++ speck/main.py
@@ -8,18 +8,21 @@ from specklepy.transports.server import ServerTransport
 from specklepy.core.api import operations
 
 
-def speck_data(API_KEY,SERVER, STREAM):
+def speck_streams(API_KEY,SERVER):
 
     # 01 CLIENT
     client = SpeckleClient(host=SERVER)
     account = get_account_from_token(API_KEY, SERVER)
     client.authenticate_with_account(account)
 
-    # 02 STREAM
+    # 02 STREAMS
     streams = client.stream.list()
-    stream = client.stream.search(STREAM)[0]
 
+    return client, streams
+
+def speck_data(STREAM, client):
     # 03 BRANCH
+    stream = client.stream.search(STREAM)[0]
     branches = client.branch.list(stream.id)
     commits = client.commit.list(stream.id, limit=100)
 
@@ -29,29 +32,19 @@ def speck_data(API_KEY,SERVER, STREAM):
     transport = ServerTransport(client=client, stream_id=stream.id)
     speckle_object = operations.receive(object_id, transport)
 
-    # # 05 VIEWER👁‍🗨
-    # def commit2viewer(stream, commit, height=800) -> str:
-    #     # embed_src = f'https://{SERVER}/embed?stream={stream.id}&commit={commit.id}&autoplay=true&filter=%7B%22propertyInfoKey%22%3A%22userStrings.{USER_STRING}%22%7D' # how to filter by a property
-    #     embed_src = f'https://{SERVER}/embed?stream={stream.id}&commit={commit.id}&autoplay=true'
-    #     return st.components.v1.iframe(src=embed_src, height=height)
-
-    # commit2viewer(stream, commits[0])
-
-    return(streams, stream, branches, commits, speckle_object)
+    return stream, branches, commits, speckle_object
 
 #////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 # 05 VIEWER👁‍🗨
 def speck_viewer(SERVER, stream, commit, height=800) -> str:
-    # embed_src = f'https://{SERVER}/embed?stream={stream.id}&commit={commit.id}&autoplay=true&filter=%7B%22propertyInfoKey%22%3A%22userStrings.{USER_STRING}%22%7D' # how to filter by a property
+
     embed_src = f'https://{SERVER}/embed?stream={stream.id}&commit={commit.id}&autoplay=true'
+
     return st.components.v1.iframe(src=embed_src, height=height)
 
-# speck_viewer(SERVER, stream, commits[0])
 
-
-
-
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
